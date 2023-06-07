ed2532815d3dae4db3ff53df5baeeb6ee422c1fc
Maria C <mariachesnokova7@gmail.com>
2023-06-07 16:07:16 -0400

    Add new Algorithms HW2.py file

---
+++ hw/Algorithms HW2.py
@@ -0,0 +1,26 @@
+# Task#1Split in Half
+def split_in_half(n):
+    center = len(n) // 2
+    first_half = n[:center]
+    second_half = n[center:]
+    if len(n) != 0:
+        first_half = n[:center + 1]
+        second_half = n[center + 1:]
+    result = second_half + first_half
+    return result
+
+
+string = 'bbbbbcaaaaa'
+print(split_in_half(string))
+
+
+# Task#2Unique Characters in String
+def unique_characters(n):
+    return len(n) == len(set(n))
+
+string1 = 'aabcde'
+string2 = 'abc'
+string3 = 'abcde'
+print(unique_characters(string1))
+print(unique_characters(string2))
+print(unique_characters(string3))
